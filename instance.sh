44d412d877225a0966b949d8fe469a6b7a0026c4
G-Dhanusha <118172980+G-Dhanusha@users.noreply.github.com>
2024-03-21 15:09:09 +0530

    Updated instance.sh

--- instance.sh
+++ instance.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-aws ec2 run-instances --image-id ami-06e4ca05d431835e9 --count 1 --instance-type t2.micro --key-name project1 --security-group-ids sg-00bec149eee274ded --region us-west-1
+aws ec2 run-instances --image-id ami-0b8b44ec9a8f90422 --count 1 --instance-type t2.micro --key-name impkey --security-group-ids sg-06e2e7509ad6231d7 --region us-west-2
 echo "$count EC2 instances have been successfully created. Please check management console"
